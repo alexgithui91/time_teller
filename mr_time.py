c38c544e26661e62c2313016a6a37f627c95ee39
alexgithui91 <alexgithui91@gmail.com>
2022-01-03 11:27:36 +0300

    code outlined

--- mr_time.py
+++ mr_time.py
@@ -1,7 +1,36 @@
 from datetime import datetime
 
-now = datetime.now()
+"""
+Tell us the current datetime as : 09:00 AM Monday, 3 August 2022
+Tell us the number of month, weeks, days to end of year
+Tell us the number of hours, minutes and seconds to end year
+See if it is possible to get Kenyan Holiday Calendar
+"""
 
-current_time = now.strftime("%Y-%m-%d %H:%M %p")
+time_now = datetime.now()
 
-print("Current time is : ", current_time)
+# current_time = now.strftime("%Y-%m-%d %H:%M %p")
+
+# print("Current time is : ", current_time)
+
+
+def breakdown_1(time_now):
+    pass
+
+
+def breakdown_2():
+    pass
+
+
+def breakdown_3():
+    pass
+
+
+def main():
+    breakdown_1(time_now)
+    breakdown_2()
+    breakdown_3()
+
+
+if __name__ == "__main__":
+    main()
