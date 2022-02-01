fb8273ba8808dc2fbd5ba4b8fe8c74e4704ba393
alexgithui91 <alexgithui91@gmail.com>
2022-02-01 12:20:15 +0300

    slight modifications to py

--- mr_time.py
+++ mr_time.py
@@ -1,16 +1,18 @@
+import datetime
 from datetime import datetime
 
 """
 Tell us the current datetime as : 09:00 AM Monday 3 August 2022
-Tell us the number of month, weeks, days to end of year
-Tell us the number of hours, minutes and seconds to end year
+Tell us the number of days, hours, minutes and seconds to end of year
 See if it is possible to get Kenyan Holiday Calendar
 """
 
 time_now = datetime.now()
+pg_break = "*" * 50
 
 
 def breakdown_1(time_now):
+    print(pg_break)
     current_date = time_now.strftime("%A, %d %B %Y")
     current_time = time_now.strftime("%H:%M %p")
 
@@ -18,8 +20,9 @@ def breakdown_1(time_now):
     print("Current Time : ", current_time)
 
 
-def breakdown_2():
-    pass
+def breakdown_2(time_now):
+    print(pg_break)
+    print(datetime.datetime("2022-12-31 23:59:59"))
 
 
 def breakdown_3():
@@ -28,7 +31,7 @@ def breakdown_3():
 
 def main():
     breakdown_1(time_now)
-    # breakdown_2()
+    breakdown_2(time_now)
     # breakdown_3()
 
 
